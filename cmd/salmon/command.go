88df66d0bcead2075f75988a383dedd0cb8eef31
Dmitry Frank <mail@dmitryfrank.com>
2026-09-15 12:34:34 +0300

    feat: show sudo guidance for privileged setup commands
    Instead of cryptic "insufficient privileges" kind of errors.

--- cmd/salmon/command.go
+++ cmd/salmon/command.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/spf13/cobra"
 
+	"github.com/dimonomid/salmon/internal/setup"
 	"github.com/dimonomid/salmon/logs"
 	"github.com/dimonomid/salmon/version"
 )
@@ -36,6 +41,9 @@ func newRootCommand() *cobra.Command {
 		Long:  "Perform the complete setup by installing the executable when needed, creating the default configuration and service account, then installing the systemd service. Run a setup subcommand to perform only one of these operations.",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, _ []string) error {
+			if err := requireRootForSalmonSetup(cmd, "", configFilename, reinstall); err != nil {
+				return err
+			}
 			if err := initializeSalmonConfig(cmd.OutOrStdout(), configFilename); err != nil {
 				return err
 			}
@@ -55,6 +63,11 @@ func newRootCommand() *cobra.Command {
 			Short: "Create the default configuration if it does not exist",
 			Args:  cobra.NoArgs,
 			RunE: func(cmd *cobra.Command, _ []string) error {
+				if configFilename == defaultSalmonConfig {
+					if err := requireRootForSalmonSetup(cmd, "create-config", configFilename, reinstall); err != nil {
+						return err
+					}
+				}
 				return initializeSalmonConfig(cmd.OutOrStdout(), configFilename)
 			},
 		},
@@ -63,6 +76,9 @@ func newRootCommand() *cobra.Command {
 			Short: "Create the system user and group used by the Salmon service",
 			Args:  cobra.NoArgs,
 			RunE: func(cmd *cobra.Command, _ []string) error {
+				if err := requireRootForSalmonSetup(cmd, "create-user", configFilename, reinstall); err != nil {
+					return err
+				}
 				return createSalmonUser(cmd.OutOrStdout())
 			},
 		},
@@ -71,6 +87,9 @@ func newRootCommand() *cobra.Command {
 			Short: "Install the executable and systemd service, then enable it",
 			Args:  cobra.NoArgs,
 			RunE: func(cmd *cobra.Command, _ []string) error {
+				if err := requireRootForSalmonSetup(cmd, "install-service", configFilename, reinstall); err != nil {
+					return err
+				}
 				return installSalmonService(cmd.OutOrStdout(), configFilename, reinstall)
 			},
 		},
@@ -78,4 +97,25 @@ func newRootCommand() *cobra.Command {
 
 	root.AddCommand(setupCommand)
 	return root
+}
+
+func requireRootForSalmonSetup(cmd *cobra.Command, operation, configFilename string, reinstall bool) error {
+	arguments := []string{setup.ShellArgument(os.Args[0]), "setup"}
+	if operation != "" {
+		arguments = append(arguments, operation)
+	}
+	if reinstall {
+		arguments = append(arguments, "--reinstall")
+	}
+	if flag := cmd.Flag("config"); flag != nil && flag.Changed {
+		arguments = append(arguments, "--config", setup.ShellArgument(configFilename))
+	}
+	return salmonSetupRootError(os.Geteuid(), strings.Join(arguments, " "))
+}
+
+func salmonSetupRootError(effectiveUserID int, invocation string) error {
+	if effectiveUserID == 0 {
+		return nil
+	}
+	return fmt.Errorf("this setup operation requires root privileges\n\nHint: Rerun it with sudo:\n\n    sudo %s", invocation)
 }
